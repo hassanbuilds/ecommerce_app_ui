88a51e77f47487d7b0c9260993729bb0b3b65225
Muhammad Hassan <mhassan.codes@gmail.com>
2026-01-19 14:33:52 +0500

    main.dart file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:ecommerce_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen()),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Lumiere')));
+    return Scaffold(appBar: AppBar(title: const Text('Lumière Home')));
   }
 }
