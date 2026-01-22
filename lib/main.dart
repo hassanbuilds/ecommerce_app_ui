6ae28947dc060cb916c1e34d4d974c17573f7b5f
Muhammad Hassan <mhassan.codes@gmail.com>
2026-01-22 09:51:01 +0500

    comment updated

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Lumiere App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const SplashScreen(), // App starts with Splash
+      home: const SplashScreen(),
     );
   }
 }
